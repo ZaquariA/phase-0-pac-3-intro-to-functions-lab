323ffc5011cee7c1303998afff4bdfb52fcab890
zaquari_andl <zaquariandl043@gmail.com>
2023-10-26 22:22:08 -0500

    functions lab

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function shout(string) {
+    return string.toUpperCase();
+    "Hello!".toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log ("HELLO");
+}
+function logWhisper (string) {
+    console.log ("hello");
+};
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === string) {
+        return "I can't hear you!";
+    } else if (string.toUpperCase() === string){
+        return "YES INDEED!";
+    }
+     else if ("Let's have dinner together!" === string){
+        return "I would love to!"
+}}
+
+
